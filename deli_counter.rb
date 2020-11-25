2b98934b292c26c7f59f1c13834f7807debe27e2
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 00:35:02 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,12 @@ def line(people)
   end
   
 end
+
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+   array.join(" and ")
+  elsif array.count >= 3
+   array.last().prepend("and ")
+   array.join(", ")
+  end
