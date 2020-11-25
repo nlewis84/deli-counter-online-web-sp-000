4506f3c8f25ed12a8eaa37c1ba35c4c485239607
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 00:48:40 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,8 +6,8 @@ katz_deli = []
 
 binding.pry
 
-def line(people)
-  if people.empty?
+def line(katz_deli)
+  if katz_deli.empty?
     puts "The line is currently empty."
   else
     puts "The line is currently:"
