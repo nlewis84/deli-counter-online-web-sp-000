74e01bf8c1ec0905d1878d42be21d688750f628f
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 00:49:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,7 +10,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    puts "The line is currently:"
+    puts "The line is currently: #{katz_deli}"
   end
 end
 
