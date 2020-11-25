1e68b58dc0531c0b9384275fa3dc20915a3b3c5e
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 01:00:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
   if katz_deli.empty?
     puts "The line is currently empty."
   else
-    katz_deli.map.with_index do |person, i|
+    katz_deli.each_with_index do |person, i|
       "#{i + 1}. #{person}"
     end.join
     puts "The line is currently: #{katz_deli}"
