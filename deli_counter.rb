42016b81240bd74f9cdb04d198f5b5ead89b7f00
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 00:34:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+
+katz_deli = []
+
+def line(people)
+  if people.empty?
+    puts "The line is currently empty."
+  else
+    
+    
+    puts "The line is currently:"
+  end
+  
+end
