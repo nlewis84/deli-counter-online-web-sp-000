e53521e8ff1d82be808eaab7b008d32301cad749
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-25 00:41:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,24 +1,26 @@
 # Write your code here.
+require 'pry'
+
 
 katz_deli = []
 
+=begin
 def line(people)
   if people.empty?
     puts "The line is currently empty."
   else
-    
-    
     puts "The line is currently:"
   end
-  
 end
+=end
 
   if people.count == 1
     people.first().prepend("1. ")
     people.join
-  elsif people.count == 2
-   people.join(" and ")
-  elsif people.count >= 3
-   people.last().prepend("and ")
+    binding.pry
+  elsif people.count >= 2
+    people.each do |index|
+      index.prepend("#{index + 1}. ")
+    end
    people.join(", ")
   end
